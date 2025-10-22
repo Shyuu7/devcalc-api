d8e21c5df9cddd1637579b48be8ffe2e46dc1aa0
Larissa Conti <laricbarros@gmail.com>
2025-10-22 19:40:44 -0300

    rollback no ErrorResponse

--- src/main/java/com/devcalc/model/ErrorResponse.java
+++ src/main/java/com/devcalc/model/ErrorResponse.java
@@ -1,11 +1,27 @@
 package com.devcalc.model;
 
 public class ErrorResponse {
-    private final String code;
-    private final String message;
+    private String code;
+    private String message;
 
     public ErrorResponse(String code, String message) {
         this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 }
